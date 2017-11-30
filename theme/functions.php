fbab17d886ee083373433384d7632ff70339733a
Robin Aaberg <robin.aaberg@knowit.no>
2017-11-30 21:06:25 +0100

    remove dup function

--- theme/functions.php
+++ theme/functions.php
@@ -7,8 +7,4 @@ add_theme_support('menus');
 function bgj_imgpath() {
     return get_template_directory_uri() . '/assets/img/';
 }
-
-function header_image() {
-    return bgj_imgpath() . 'header2.png';
-}
 ?>
