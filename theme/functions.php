082c8c9e7b1a35aa0f993d38c041c38e5bbdb45a
Robin Aaberg <robin.aaberg@knowit.no>
2017-11-30 21:05:17 +0100

    add header image

--- theme/functions.php
+++ theme/functions.php
@@ -3,4 +3,12 @@
 // Adds menu and widgets
 add_theme_support('menus');
 
+
+function bgj_imgpath() {
+    return get_template_directory_uri() . '/assets/img/';
+}
+
+function header_image() {
+    return bgj_imgpath() . 'header2.png';
+}
 ?>
