cfbe8f9321fce7df919f4f40f8c4cc1baecb6e0b
Robin Aaberg <robin.aaberg@knowit.no>
2017-12-06 21:16:49 +0100

    moved reset.css to assets/css and included it in header.php

--- theme/functions.php
+++ theme/functions.php
@@ -8,4 +8,9 @@ add_theme_support( 'custom-header' );
 function bgj_imgpath() {
     return get_template_directory_uri() . '/assets/img/';
 }
+function bgj_csspath() {
+    return get_template_directory_uri() . '/assets/css/';
+}
+
+
 ?>
